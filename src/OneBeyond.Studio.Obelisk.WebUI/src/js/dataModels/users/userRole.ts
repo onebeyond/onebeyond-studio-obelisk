fae376cebd99f8b90a65ac42e4ace8f04d90c2a2
Andrii Kaplanovskyi <andrii.kaplanovskyi@dcslsoftware.com>
2023-02-28 15:29:44 +0000

    WebUI removed

--- src/OneBeyond.Studio.Obelisk.WebUI/src/js/dataModels/users/userRole.ts
+++
@@ -1,9 +0,0 @@
-export class UserRole {
-    public static readonly USER = "User"
-    public static readonly ADMINISTRATOR = "Administrator"
-
-    public static AllRoles = [
-        UserRole.ADMINISTRATOR,
-        UserRole.USER
-    ]
-}
--- src/OneBeyond.Studio.Obelisk.WebUI/src/js/util/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ShortDate = "dd/MM/yyyy";
-export const LongDateTime = "dd/MM/yyyy HH:mm";
-export const MonthYearDate = "MMM yy";
