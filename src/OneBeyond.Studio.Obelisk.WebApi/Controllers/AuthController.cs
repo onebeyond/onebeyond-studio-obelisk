b36254e229636641353ff292bd25e31eab9b9c2e
aslam-mohammed-onebeyond <aslam.mohammed@one-beyond.com>
2023-06-20 09:57:11 +0100

    ex messaged used for log information

--- src/OneBeyond.Studio.Obelisk.WebApi/Controllers/AuthController.cs
+++ src/OneBeyond.Studio.Obelisk.WebApi/Controllers/AuthController.cs
@@ -119,7 +119,7 @@ public sealed class AuthController : ControllerBase
         catch (Exception ex)
         {
             // Don't throw exception so we don't reveal which emails are currently in use
-            Logger.LogInformation("Failed to reset user password");
+            Logger.LogInformation($"Failed to reset user password: {ex.Message}");
         }
     }
 
