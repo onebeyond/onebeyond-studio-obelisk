0adffc02940d11c659cf43c4796b4fbc153963a6
Nick Skliar-Davies <nick.skliar-davies@one-beyond.com>
2025-03-24 13:39:55 +0000

    refactor: change following code review

--- src/OneBeyond.Studio.Obelisk.WebApi/Dto/SignInJwtDto.cs
+++ src/OneBeyond.Studio.Obelisk.WebApi/Dto/SignInJwtDto.cs
@@ -1,24 +1,21 @@
-﻿using EnsureThat;
+using EnsureThat;
 
 namespace OneBeyond.Studio.Obelisk.WebApi.Controllers;
-
-public sealed partial class JWTAuthenticationController
+public sealed record SignInJwtDto
 {
-    public sealed record SignInJwtDto
+    public SignInJwtDto(
+        string username,
+        string password)
     {
-        public SignInJwtDto(
-            string username,
-            string password)
-        {
-            EnsureArg.IsNotNullOrWhiteSpace(username, nameof(username));
-            EnsureArg.IsNotNullOrWhiteSpace(password, nameof(password));
+        EnsureArg.IsNotNullOrWhiteSpace(username, nameof(username));
+        EnsureArg.IsNotNullOrWhiteSpace(password, nameof(password));
 
-            Username = username;
-            Password = password;
-        }
-
-        public string Username { get; private init; }
-        public string Password { get; private init; }
+        Username = username;
+        Password = password;
     }
 
+    public string Username { get; private init; }
+    public string Password { get; private init; }
 }
+
+
