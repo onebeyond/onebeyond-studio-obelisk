efd887ba833d2268515d279864ef8b68bf534bbd
Nick Skliar-Davies <nick.skliar-davies@one-beyond.com>
2025-03-24 12:22:58 +0000

    sys: infrastructure updates with JWT at the forefront

--- devops/Terraform/locals.tf
+++ devops/Terraform/locals.tf
@@ -8,6 +8,7 @@ locals {
     web_api_sqldb_user_password     = "WebApiSqlDbUserPassword"
     web_api_sqldb_connection_string = "WebApiSqlDbConnectionString"
     web_api_admin_password          = "WebApiAdminPassword"
+    jwt_secret                      = "JwtSecret"
   }
   default_tags = {
     stage      = local.stage_code
