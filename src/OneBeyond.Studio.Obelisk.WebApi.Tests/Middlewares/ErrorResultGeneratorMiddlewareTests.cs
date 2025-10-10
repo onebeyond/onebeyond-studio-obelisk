f63fd441c8787c3eb2dbeb33ee346dfdc93ad37f
blelkes-ob <balint.lelkes@one-beyond.com>
2025-10-10 10:31:53 +0200

    Add Directory.Build.props for managing common project properties. Add test projects to central package management.

--- src/OneBeyond.Studio.Obelisk.WebApi.Tests/Middlewares/ErrorResultGeneratorMiddlewareTests.cs
+++ src/OneBeyond.Studio.Obelisk.WebApi.Tests/Middlewares/ErrorResultGeneratorMiddlewareTests.cs
@@ -1,7 +1,5 @@
 using System.Net;
 using System.Net.Http.Json;
-using FluentAssertions;
-using FluentAssertions.Execution;
 using Microsoft.AspNetCore.Mvc;
 using OneBeyond.Studio.Obelisk.WebApi.Middlewares;
 
